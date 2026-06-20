29fa144263f1ea798268cd7e9795fb1d15680dbd
jhon sarabia <jsa.26@hotmail.com>
2026-06-20 13:45:06 -0500

    PWA Android recarga automatica nueva version

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,12 @@
+// Forzar activacion inmediata del service worker en Android PWA
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
 importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js');
 
